cc2428c0658296e2d562828d14e7193602d0fc6e
BrettLamb <crossgumi@proton.me>
2026-09-13 20:42:53 -0700

    change_accumulator_variable_name

--- Programming_Exercise_1.py
+++ Programming_Exercise_1.py
@@ -14,14 +14,14 @@ def accept_purchase(tickets_left):
 
 def manage_ticket_sales():
     total_tickets = 10 #taking the total number of tickets down from 20 to 10
-    buyer_count = 0
+    buyer_accumulator = 0 #variable used to be named  buyer_count
     while total_tickets > 0:
         tickets_bought = accept_purchase(total_tickets)
         total_tickets -= tickets_bought
-        buyer_count += 1
+        buyer_accumulator += 1
         print (f'Number of tickets left is: {total_tickets}')
 
     print('All the tickets are gone!')
-    print(f'The total number of buyers was: {buyer_count}')
+    print(f'The total number of buyers was: {buyer_accumulator}')
 
 manage_ticket_sales()
